b961d3aa991d855bf173c500dba4490a43d41cbe
hoangviethung <hoangviet.hung.009@gmail.com>
2021-03-25 02:05:20 +0700

    done pricing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,21 @@
 import Tab from './libraries/tabs';
 
+const fixedHeaderWhenScroll = () => {
+	const header = document.querySelector('header');
+	if (window.pageYOffset > header.offsetHeight) {
+		header.classList.add('hvh-scrolled');
+	} else {
+		header.classList.remove('hvh-scrolled');
+	}
+};
+
 window.addEventListener('load', (e) => {
+	fixedHeaderWhenScroll();
+	let PricingBoard = new Tab('.hvh-pricing-board .tab-container');
 });
 
 window.addEventListener('scroll', (e) => {
+	fixedHeaderWhenScroll();
 });
 
-window.addEventListener('resize', () => {
-});
+window.addEventListener('resize', () => {});
