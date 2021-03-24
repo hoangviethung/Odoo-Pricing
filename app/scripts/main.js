1d0c3eb87d101cadd89ae2753247c325033f89d5
hoangviethung <hoangviet.hung.009@gmail.com>
2021-03-24 20:50:17 +0700

    update source

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+import Tab from './libraries/tabs';
+
+window.addEventListener('load', (e) => {
+});
+
+window.addEventListener('scroll', (e) => {
+});
+
+window.addEventListener('resize', () => {
+});
